db90e5f257fda92ce429b17f40ba7c2c46c106f7
yogessstha <yogessstha36@gmail.com>
2021-03-17 16:35:55 +0545

    task2

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-#Activities and intents
-![](screenrecord2.gif)
+<h1>Activities and intents</h1>
+<img src="screenshots1.png" width="50%">
+
 
--- app/src/main/java/com/example/twoactivities/MainActivity.java
+++ app/src/main/java/com/example/twoactivities/MainActivity.java
@@ -2,22 +2,32 @@ package com.example.twoactivities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String EXTRA_MESSAGE =
+            "com.example.android.twoactivities.extra.MESSAGE";
     private static final String LOG_TAG =
             MainActivity.class.getSimpleName();
+    private EditText mMessageEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mMessageEditText = findViewById(R.id.editText_main);
     }
 
     public void launchSecondActivity(View view) {
-        Log.d(LOG_TAG,"Button clicked!");
+        Log.d(LOG_TAG, "Button clicked!");
+        Intent intent = new Intent(this, SecondActivity.class);
+        String message = mMessageEditText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
